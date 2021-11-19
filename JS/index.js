b58650444ea0c600fd6fbf34e1c77186f5cc3e15
leepowe <leepowell2@yahoo.com>
2021-11-18 19:10:12 -0500

    added about me, contact, and project functions to index.js and a-z

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,54 @@
-import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import Header from './components/Header.js';
 import AboutMe from './pages/AboutMe.js';
+import Contact from './pages/Contact.js';
+import Projects from './pages/Projects.js';
 
 buildPage();
 
-function buildPage (){
-    navigateToAboutMe();
-    header();
+function buildPage() {
     footer();
+    header();
+    navigateToAboutMe();
+    navigateToContact();
+    navigateToProjects();
 }
 
 const app = document.querySelector('#app');
 
+function footer() {
+  const footerElement = document.querySelector(".footer");
+  footerElement.innerHTML = Footer();
+}
+
 function header() {
     const headerElement = document.querySelector(".header");
     headerElement.innerHTML = Header();
+  }  
+
+  function navigateToAboutMe() {
+    const aboutMeButton = document.querySelector('.nav__list_aboutMe');
+    aboutMeButton.addEventListener('click', ()=> {
+      console.log('about me page fired');
+      let app = document.querySelector('#app');
+      app.innerHTML = AboutMe();
+    });
   }
-  
-  function footer() {
-    const footerElement = document.querySelector(".footer");
-    footerElement.innerHTML = Footer();
+
+  function navigateToContact() {
+    const contactButton = document.querySelector('.nav__list_contact');
+    contactButton.addEventListener('click', ()=> {
+      console.log('contact page fired');
+      let app = document.querySelector('#app');
+      app.innerHTML = Contact();
+    });
+  }
+
+  function navigateToProjects() {
+    const projectsButton = document.querySelector('.nav__list_projects');
+    projectsButton.addEventListener('click', ()=> {
+      console.log('projects page fired');
+      let app = document.querySelector('#app');
+      app.innerHTML = Projects();
+    });
   }
