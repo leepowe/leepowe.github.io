8a58e0ae635a5c7ddbefb4b475abaca1f73e169c
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-28 01:03:47 -0500

    Clean code

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
 const themeDots = document.querySelectorAll('.theme-dot');
-const theme = localStorage.getItem('theme') || 'light';
+const themeStyle = document.getElementById('theme-style');
+const fontFamily = document.getElementById('font-family');
 const readabilityBtn = document.querySelector('.readability-btn');
-let readability = localStorage.getItem('readability') || 'stylized';
 
-setTheme(theme);
-setReadability(readability);
-
-for (const themeDot of themeDots) {
-	themeDot.addEventListener('click', () => {
-		const mode = themeDot.dataset.mode;
-		setTheme(mode);
-	});
-}
-
-readabilityBtn.addEventListener('click', () => {
-	readabilityBtn.classList.toggle('easy-read');
-	readability = readabilityBtn.classList.contains('easy-read')
-		? 'readability'
-		: 'stylized';
-	setReadability(readability);
-});
+const DEFAULT_THEME = 'light';
+const STYLIZED_FONT = 'stylized';
+const READABILITY_FONT = 'readability';
+const LOCAL_STORAGE_THEME_KEY = 'theme';
+const LOCAL_STORAGE_READABILITY_KEY = 'readability';
 
 function getStylesheetUrl(mode) {
-	return `css/${mode}.css`;
+  return `css/${mode}.css`;
 }
 
 function setTheme(mode) {
-	let themeStyle = document.getElementById('theme-style'),
-		defaultTheme = getStylesheetUrl('light');
-
-	switch (mode) {
-		case 'light':
-		case 'blue':
-		case 'green':
-		case 'purple':
-			themeStyle.href = getStylesheetUrl(mode);
-			break;
-		default:
-			themeStyle.href = defaultTheme;
-			mode = 'light';
-			break;
-	}
-	localStorage.setItem('theme', mode);
+  const themeUrl = mode === DEFAULT_THEME ? getStylesheetUrl(DEFAULT_THEME) : getStylesheetUrl(mode);
+  themeStyle.href = themeUrl;
+  localStorage.setItem(LOCAL_STORAGE_THEME_KEY, mode);
 }
 
-function setReadability(readability) {
-	let fontFamily = document.getElementById('font-family');
-	let stylizedFont = getStylesheetUrl('stylized');
+function setReadability() {
+  const isEasyRead = readabilityBtn.classList.toggle('easy-read');
+  const fontUrl = isEasyRead ? getStylesheetUrl(READABILITY_FONT) : getStylesheetUrl(STYLIZED_FONT);
+  const buttonText = isEasyRead ? 'Fancy Font' : 'Easy Read';
 
-	if (readability === 'readability') {
-		fontFamily.href = getStylesheetUrl('readability');
-		readabilityBtn.innerHTML = 'Fancy Font';
-	} else {
-		fontFamily.href = stylizedFont;
-		readabilityBtn.innerHTML = 'Easy Read';
-	}
-	localStorage.setItem('readability', readability);
+  fontFamily.href = fontUrl;
+  readabilityBtn.innerHTML = buttonText;
+
+  const readabilityMode = isEasyRead ? READABILITY_FONT : STYLIZED_FONT;
+  localStorage.setItem(LOCAL_STORAGE_READABILITY_KEY, readabilityMode);
 }
+
+for (const themeDot of themeDots) {
+  themeDot.addEventListener('click', () => {
+    const mode = themeDot.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+readabilityBtn.addEventListener('click', setReadability);
+
+const savedTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
+const savedReadability = localStorage.getItem(LOCAL_STORAGE_READABILITY_KEY) || STYLIZED_FONT;
+
+setTheme(savedTheme);
+setReadability(savedReadability);
